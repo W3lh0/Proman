3cdfc5c458ddd4f259f9b9c26da91a6539d4f02b
W3lh0 <e2101506@edu.vamk.fi>
2025-07-30 13:48:12 +0300

    feat: create Connection class

--- model/connection.php
+++ model/connection.php
@@ -1,14 +1,37 @@
 <?php
 
-function dbConnect()
+class Connection 
 {
-    try {
-        require "config.php";
+    private string $host;
+    private string $userName;
+    private string $password;
+    private string $dbName;
+    private array $options;
+    private PDO $dbconnection;
 
-        $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
-    } catch (PDOException $err) {
-        echo "Database connection error. <br>" . $err->getMessage();
-        exit;
+    public function __constructor(string $host, string $dbName, string $userName, string $password, array $options = []) 
+    {
+        $this->host = $host;
+        $this->userName = $userName;
+        $this->password = $password;
+        $this->dbName = $dbName;
+        $this->$option = $options;
+
+        $dns = "mysql:host{$this->host};dbname{$this->dbName};charset=utf8mb4";
+
+        try {
+            $this->pdo = new PDO($dns, $this->userName, $this->password, $this->options);
+
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        } catch (PDOException $err) {
+            die("Database connection failed: " . $err->getMessage());
+        }
     }
-    return $connection;
+
+    public function getConnection(): PDO
+    {
+        return $this->dbconnection;
+    }
 }
