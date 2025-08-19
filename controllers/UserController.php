bc746005ddd244b5bdb72a29dce1c30266eadaa9
W3lh0 <e2101506@edu.vamk.fi>
2025-08-19 08:15:59 +0300

    feat: Create UserController class

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -26,26 +26,63 @@ class UserController
 
     public function logout(): void
     {
-
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        session_unset();
+        session_destroy();
+        echo "Log out successful";
     }
 
     public function register(string $email, string $password): void
     {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
+        $success = $this->userModel->createUser($email, $hashedPassword);
+
+        if ($success) {
+            echo "Registered user successfuly";
+        } else {
+            echo "Error while trying to register user, user with this email might exist allready";
+        }
     }
 
     public function showProfile(int $userId): void
     {
+        $user = $this->userModel->getUserId($userId);
 
+        if ($user) {
+            echo "User: " . $user['email'];
+        } else {
+            echo "User not found.";
+        }
     }
 
     public function updateProfile(int $userId, string $email, string $password): void
     {
+        $hashedPassword = null;
 
+        if ($password !== null && !empty($password)) {
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        }
+
+        $success = $this->userModel->updateUser($userId, $email, $hashedPassword);
+
+        if ($success) {
+            echo "Profile updated successfuly";
+        } else {
+            echo "Error while trying to update profile";
+        }
     }
 
     public function deleteProfile(int $userId): void
     {
+        $success = $this->userModle->deleteUser($userId);
 
+        if ($success) {
+            echo "User deleted successfuly";
+        } else {
+            echo "Error while trying to delete User";
+        }
     }
 }
