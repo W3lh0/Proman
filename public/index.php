5396b43bfde0faa99e688af2b077829d53961109
W3lh0 <e2101506@edu.vamk.fi>
2025-08-29 09:48:06 +0300

    fix: Remove error message

--- public/index.php
+++ public/index.php
@@ -10,8 +10,6 @@ spl_autoload_register(function ($className) {
     $filePath = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
     $fullPath = ROOT_PATH . DIRECTORY_SEPARATOR . $filePath;
 
-    echo "yritetään ladata: $filePath<br>";
-
     if (file_exists($fullPath)) {
         require_once $fullPath;
     }
